26d7dc2208050795663860e8ad9836e09a76ac7d
Davis Nelson <davis.nelson27@gmail.com>
2020-01-24 12:35:46 -0800

    Finished navbar, download buttons, and pdf display

--- change_picture.js
+++ change_picture.js
@@ -1,2 +1,13 @@
-document.getElementById("cloud_btn").addEventListener("click", function(){ document.getElementById("diagram").src = "assets/cloud.png" });
-document.getElementById("onsite_btn").addEventListener("click", function(){ document.getElementById("diagram").src = "assets/onsite.png" });
+const cloud_btn = document.getElementById("cloud_btn");
+const onsite_btn = document.getElementById("onsite_btn");
+const writeup = document.getElementById("writeup");
+const diagram = document.getElementById("diagram");
+
+cloud_btn.addEventListener("click", function(){ 
+    diagram.src = "assets/cloud.png";
+    writeup.src = "assets/cloud_writeup.pdf";
+});
+onsite_btn.addEventListener("click", function(){ 
+    diagram.src = "assets/onsite.png" 
+    writeup.src = "assets/onsite_writeup.pdf";
+});
